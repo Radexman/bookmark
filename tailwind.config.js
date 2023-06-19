4491567df26166861537a34cf01ef51c86c22d88
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-06-19 18:21:46 +0200

    Initial Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
 	content: ['dist/*html'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				softBlue: 'hsl(231, 69%, 60%)',
+				softRed: 'hsl(0, 94%, 66%)',
+				grayishBlue: 'hsl(229, 8%, 60%)',
+				veryDarkBlue: 'hsl(229, 31%, 21%)',
+			},
+			screens: {
+				sm: '375px',
+				lg: '1440px',
+			},
+			fontFamily: {
+				sans: ['Rubik', 'sans-serif'],
+			},
+		},
 	},
 	plugins: [],
 };
