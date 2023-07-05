51b4daa7de9ff9d8f168fc6613c4cdd767384aec
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-07-05 13:51:41 +0200

    Form Validation

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,10 @@
 const tabs = document.querySelectorAll('.tab');
 const panels = document.querySelectorAll('.panel');
+const newsletterForm = document.getElementById('newsletter-form');
+const errorMsg = document.getElementById('error-msg');
+const input = document.getElementById('input');
+const errorMark = document.getElementById('error-mark');
+let regex = new RegExp('[a-z0-9]+@[a-z]+.[a-z]{2,3}');
 
 const onTabClick = (e) => {
 	// Deactivate all tabs
@@ -16,5 +21,25 @@ const onTabClick = (e) => {
 	document.getElementById('panels').getElementsByClassName(classString)[0].classList.remove('hidden');
 };
 
+// Form Validation
+
+const validateForm = (e) => {
+	e.preventDefault();
+	let emailInput = e.target.firstElementChild.firstElementChild.value;
+
+	if (emailInput === '' || regex.test(emailInput) === false) {
+		errorMsg.classList.remove('hidden');
+		input.classList.remove('border-white');
+		input.classList.add('border-2', 'border-softRed');
+		errorMark.classList.remove('hidden');
+	} else {
+		errorMsg.classList.add('hidden');
+		input.classList.remove('border-2', 'border-softRed');
+		input.classList.remove('border-white');
+		errorMark.classList.add('hidden');
+	}
+};
+
 // Tabs menu event listener
 tabs.forEach((tab) => tab.addEventListener('click', onTabClick));
+newsletterForm.addEventListener('submit', validateForm);
